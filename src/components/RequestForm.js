f17bc7e7921e530297a0efcc8287e47134ccb71c
Dovydas Girskas <dovydas.girskas@mif.stud.vu.lt>
2024-08-15 11:17:56 +0300

    formos patobulinimai

--- src/components/RequestForm.js
+++ src/components/RequestForm.js
@@ -1,22 +1,61 @@
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import { Container, Row, Col } from "react-bootstrap";
+
+import "../styles/ContainerSize.css";
 
 export function RequestForm() {
-    return(
-        <div>
-            <p class="display-5">Lizingo forma:</p>
-            <p class="mb-5">Užpildykite šią formą ir laukite mūsų atsakymo. Peržiūrėję jūsų užklausą, pakeisime paraiškos statusą <strong>Paraiškos</strong> skiltyje</p>
+  return (
+    <div>
+      <Container className="Container-sizing">
+        <p class="display-5">Lizingo forma:</p>
+        <p class="mb-5">
+          Užpildykite šią formą ir laukite mūsų atsakymo. Peržiūrėję jūsų
+          užklausą, pakeisime paraiškos statusą skiltyje{" "}
+          <strong>Paraiškos</strong>{" "}
+        </p>
 
-            <Form>
-                <Form.Group>
-                    <Form.Label>Mašinos kaina</Form.Label>
-                    <Form.Control type='number' placeholder='0' />
-                    <Form.Text>nuo 7000 iki 169000</Form.Text>
-                </Form.Group>
-                <Button>
+        <Form>
+          <Row className="mb-4">
+            <Col md={6}>
+              <Form.Group>
+                <Form.Label>Mašinos kaina</Form.Label>
+                <Form.Control type="number" placeholder="0" />
+                <Form.Text>nuo 7000 Eur. iki 169000 Eur.</Form.Text>
+              </Form.Group>
+            </Col>
+            <Col md={6}>
+              <Form.Group>
+                <Form.Label>Laikotarpis</Form.Label>
+                <Form.Control type="number" placeholder="0" />
+                <Form.Text>nuo 6 mėn. iki 96 mėn.</Form.Text>
+              </Form.Group>
+            </Col>
+          </Row>
 
-                </Button>
-            </Form>
-        </div>
-    );
+          <Row className="mb-5">
+            <Col md={6}>
+              <Form.Group>
+                <Form.Label>Pradinis įnašas</Form.Label>
+                <Form.Control type="number" placeholder="0" />
+                <Form.Text>10% automobilio sumos</Form.Text>
+              </Form.Group>
+            </Col>
+            <Col md={6}>
+              <Form.Group>
+                <Form.Label>Pradinis įnašas %</Form.Label>
+                <Form.Control type="number" placeholder="0" />
+              </Form.Group>
+            </Col>
+          </Row>
+
+          <Row>
+            <Col md="auto">
+              <Button>Pateikti</Button>
+            </Col>
+          </Row>
+        </Form>
+      </Container>
+    </div>
+  );
 }
