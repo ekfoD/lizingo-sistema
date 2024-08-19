bd17a06b35d4eaf36cac1265bc7819665b1a2d80
Dovydas Girskas <dovydas.girskas@mif.stud.vu.lt>
2024-08-20 00:59:50 +0300

    pradeti tvarkyti atskiros paraisku langai

--- src/components/Request.js
+++ src/components/Request.js
@@ -1,7 +1,22 @@
-export function Request() {
-    return(
-        <div>
-            <h3>it is a seperate Request</h3>
-        </div>
-    );
+import { Container, Row, Col, Button } from "react-bootstrap";
+
+export function Request({ moneyAmount, duration, contribution }) {
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <p>vardas</p>
+        </Col>
+        <Col className="d-flex align-content-center justify-content-end">
+          <p>status</p>
+        </Col>
+        <Col className="d-flex align-content-end justify-content-end">
+          <Button className="btn-danger">Atšaukti</Button>
+        </Col>
+        <Col>
+          <Button className="btn-success">Peržiūrėti</Button>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
--- src/components/RequestList.js
+++ src/components/RequestList.js
@@ -1,11 +1,25 @@
 import { Request } from "./Request";
 
+import { Container, ListGroup } from "react-bootstrap";
+
+import "../styles/ContainerSize.css";
+
 export function RequestList() {
-    return(
-        <div>
-            <h2>RequestList</h2>
+  return (
+    <div>
+      <Container className="Container-sizing">
+        <p className="display-5">Paraiškų sąrašas:</p>
+
+        <ListGroup>
+          <ListGroup.Item variant="primary" className="my-2">
+            {" "}
+            <Request moneyAmount={100} duration={10} contribution={10} />
+          </ListGroup.Item>
+          <ListGroup.Item variant="primary" className="my-2">
             <Request />
-            <Request />
-        </div>
-    );
+          </ListGroup.Item>
+        </ListGroup>
+      </Container>
+    </div>
+  );
 }
