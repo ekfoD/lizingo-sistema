a9f5b56132e2424435a6d5c9146ebf44bce966b7
Dovydas Girskas <dovydas.girskas@mif.stud.vu.lt>
2024-09-04 14:57:40 +0300

    ištrinti nereikalingi failai

--- src/components/About.js
+++
@@ -1,8 +0,0 @@
-export function About() {
-    return(
-        <div class="text-center">
-            <p class="display-4">Sveiki atvykę!</p>
-            <p class="text">Lizingas mašinoms - taip!</p>
-        </div>
-    );
-}
