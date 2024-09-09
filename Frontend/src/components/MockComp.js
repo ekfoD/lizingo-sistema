6c49c0467d47379fb8db4673c81b5d94654f3c44
Dovydas Girskas <dovydas.girskas@mif.stud.vu.lt>
2024-09-09 23:21:16 +0300

    meginta suconnectinti API su frontend

--- Frontend/src/components/MockComp.js
+++ Frontend/src/components/MockComp.js
@@ -3,36 +3,37 @@ import { Button } from "react-bootstrap";
 
 export function MockComp() {
   const [posts, setPosts] = useState([]);
+  const [weathers, setWeathers] = useState([]);
 
-  const getPosts = () => {
-    fetch("https://jsonplaceholder.typicode.com/posts?_limit=4")
+  const getWeathers = () => {
+    fetch("http://localhost:5140/weatherforecast/")
       .then((response) => response.json())
-      .then((data) => setPosts(data));
+      .then((data) => setWeathers(data));
   };
 
-  const addPost = (title, body) => {
-    fetch("https://jsonplaceholder.typicode.com/posts", {
-      method: "POST",
-      body: JSON.stringify({
-        title: title,
-        body: body,
-        userId: Math.random().toString(36).slice(2),
-      }),
-      headers: {
-        "Content-type": "application/json; charset=UTF-8",
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => setPosts((prevPosts) => [data, ...prevPosts]));
-  };
+  //   const addPost = (title, body) => {
+  //     fetch("https://jsonplaceholder.typicode.com/posts", {
+  //       method: "POST",
+  //       body: JSON.stringify({
+  //         title: title,
+  //         body: body,
+  //         userId: Math.random().toString(36).slice(2),
+  //       }),
+  //       headers: {
+  //         "Content-type": "application/json; charset=UTF-8",
+  //       },
+  //     })
+  //       .then((response) => response.json())
+  //       .then((data) => setPosts((prevPosts) => [data, ...prevPosts]));
+  //   };
 
   const handleAddRequest = (e) => {
     e.preventDefault();
-    addPost("yeet", "yaa");
+    //addPost("yeet", "yaa");
   };
 
   useEffect(() => {
-    getPosts();
+    getWeathers();
   }, []);
 
   return (
@@ -40,11 +41,11 @@ export function MockComp() {
       <h1>okay mock stuff works</h1>
       <Button onClick={(e) => handleAddRequest(e)}>click meeee</Button>
 
-      {posts.map((post) => {
+      {weathers.map((weather) => {
         return (
           <>
-            <h5>{post.title}</h5>
-            <p5>{post.body}</p5>
+            <h5>{weather.TemperatureC}</h5>
+            <p5>{weather.Summary}</p5>
           </>
         );
       })}
