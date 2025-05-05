b6afa397c7239854df35cb0ce3e8cc9e97fd51d7
Dovydas Girskas <dovydas.girskas@mif.stud.vu.lt>
2025-05-05 10:49:52 +0300

    created higher hierarchy level .gitignore

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,9 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// https://www.c-sharpcorner.com/article/cross-origin-resource-sharing-cors-in-net-8/
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CustomPolicy", builder =>
+        builder.WithOrigins()
+               .AllowAnyMethod()
+               .AllowAnyHeader());
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers(); // Enable MVC Controllers
+
 
 var app = builder.Build();
 
@@ -14,31 +24,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors("CustomPolicy");
 app.UseHttpsRedirection();
-
-// var summaries = new[]
-// {
-//     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-// };
-
-// app.MapGet("/weatherforecast", () =>
-// {
-//     var forecast =  Enumerable.Range(1, 5).Select(index =>
-//         new WeatherForecast
-//         (
-//             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-//             Random.Shared.Next(-20, 55),
-//             summaries[Random.Shared.Next(summaries.Length)]
-//         ))
-//         .ToArray();
-//     return forecast;
-// })
-// .WithName("GetWeatherForecast")
-// .WithOpenApi();
+app.MapControllers();
 
 app.Run();
-
-// record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-// {
-//     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-// }
